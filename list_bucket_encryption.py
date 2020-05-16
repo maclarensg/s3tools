1711e8dcaba8f22c1fb019ec57c6a9d07e88e8f3
Gavin <maclarensg>
2020-05-16 08:33:44 +0800

    asyncio implemented

--- list_bucket_encryption.py
+++ list_bucket_encryption.py
@@ -3,11 +3,36 @@ import logging
 import boto3
 import sys, argparse
 import yaml
+import asyncio
 from botocore.exceptions import ClientError
 
 
+async def retrieveSecurityPolicy(s3, buckets, bucket):
+    try:
+        # Get each buck name encryption
+        response = s3.get_bucket_encryption(
+            Bucket=bucket
+        )
+
+        # Print indicator when a bucket has response
+        print("." , end="", flush=True)
+
+        # When a encyption info is found save to dictionary 
+        if "ServerSideEncryptionConfiguration" in response and "Rules" in response["ServerSideEncryptionConfiguration"] and len(response["ServerSideEncryptionConfiguration"]["Rules"]) > 0:
+            for rule in response["ServerSideEncryptionConfiguration"]["Rules"]:
+               if "ApplyServerSideEncryptionByDefault" in rule and "SSEAlgorithm" in rule["ApplyServerSideEncryptionByDefault"]:
+                    if rule["ApplyServerSideEncryptionByDefault"]["SSEAlgorithm"] == "aws:kms":
+                        del buckets[bucket]
+                    else: 
+                        buckets[bucket] = response["ServerSideEncryptionConfiguration"]
+
+    except ClientError:
+        pass
+        # print('Bucket %s has no bucket encryption configured' % bucket) 
+
+
 # Main Program
-def main():
+async def main():
 
     # Argument parser
     parser = argparse.ArgumentParser(description='List and audit S3 Encryption buckets and save ServerSideEncryptionConfiguration.', 
@@ -31,29 +56,19 @@ def main():
         # Convert the response to a dictionary of buckets names as key to None value. value will set to 'ServerSideEncryptionConfiguration' if one has found
         buckets = ({ bucket["Name"]:None for bucket in response['Buckets'] }) 
         
+        # Create Async tasks list
+        async_tasks = []
+
         # Iterate the bucket keys
         for bucket in list(buckets):
-            print(".", end='', flush=True)
-            try:
-                # Get each buck name encryption
-                response = s3.get_bucket_encryption(
-                    Bucket=bucket
+            print("Create task for %s" % bucket)
+            async_tasks.append( 
+                loop.create_task( retrieveSecurityPolicy(s3, buckets, bucket )) 
                 )
 
-                # When a encyption info is found save to dictionary 
-                if "ServerSideEncryptionConfiguration" in response and "Rules" in response["ServerSideEncryptionConfiguration"] and len(response["ServerSideEncryptionConfiguration"]["Rules"]) > 0:
-                    for rule in response["ServerSideEncryptionConfiguration"]["Rules"]:
-                       if "ApplyServerSideEncryptionByDefault" in rule and "SSEAlgorithm" in rule["ApplyServerSideEncryptionByDefault"]:
-                            if rule["ApplyServerSideEncryptionByDefault"]["SSEAlgorithm"] == "aws:kms":
-                                del buckets[bucket]
-                            else: 
-                                buckets[bucket] = response["ServerSideEncryptionConfiguration"]
-
-            except ClientError:
-                pass
-                # print('Bucket %s has no bucket encryption configured' % bucket) 
-
-        print("Done")
+        print("Please wait while background task is completed")
+        await asyncio.wait(async_tasks)
+        
         
         # Finally dump result to file
         with open(f'{args.save}', 'w') as file:
@@ -67,4 +82,11 @@ def main():
 
 # Progam Start
 if __name__ == "__main__":
-    main()
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+    except Exception as e:
+        logging.error(e)
+    finally:
+        loop.close()
+    
